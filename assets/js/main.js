5f398826994d078c90bbda0fdc527f2a008d1d5c
jjwedemyer <mail@jakob-wedemyer.de>
2017-12-23 02:40:44 +0100

    almost done removing jquery

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,47 +15,41 @@ const backgroundColors = {
 const date = new Date();
 let task;
 
-let refreshTasks = function() {
+const refreshTasks = () => {
 
-    $.getJSON('https://quest.meeka.org/list5', function(data) {
+    fetch('https://quest.meeka.org/list5')
+    .then((res)=>{
+        if(!res.ok)
+            throw new Error("Request failed with a HTTP Error Code");
+        return res.json();
+    })
+    .then((json)=>{
             for(let obj of data){
                 tasks.push(obj);
             }
             console.log('tasks list was refreshed successfully');
-        })
-        .done(function() {})
-        .fail(function(jqxhr, textStatus, error) {
-            console.log('There was an error fetching new compliments: ' + textStatus);
-        })
-        .always(function() {
-            showNewCompliment();
-        });
+    })
+    .then(showNewCompliment());
 }
 
-let showNewCompliment = function() {
+const showNewCompliment = () => {
     task = tasks.shift();
-    $.getJSON('https://quest.meeka.org/rquest',function(data){
+    fetch('https://quest.meeka.org/rquest')
+    .then((res)=>{
+        if(!res.ok)
+            throw new Error('There was an error fetching the new quest: ' + textStatus + " error:"+error);
+        return res.json();
+    })
+    .then((data)=>{
         tasks.push(data);
-    }).done(function(){console.log("new task appended!");})
-    .fail(function(jqxhr, textStatus, error) {
-            console.log('There was an error fetching the new quest: ' + textStatus + " error:"+error);
-        });
+    });
     setBackground(task);
     $('.title').text(task.title);
-    // Set link path here
     $('.quest').text(task.task);
 }
 
-let setBackground = function(task) {
+const setBackground = (task) => {
     let bgcolor = backgroundColors[task.color];
-    $('body').css('background-color', bgcolor);
-    $('.head-entry').css('background-color', bgcolor);
-}
-
-
-
-let setRandomBackground = function() {
-    let bgcolor = backgroundColors[Math.floor(Math.random() * backgroundColors.length)];
     $('body').css('background-color', bgcolor);
     $('.head-entry').css('background-color', bgcolor);
 }
@@ -64,14 +58,14 @@ let setRandomBackground = function() {
 
 let menu_state = false;
 
-let menu_open = function(){
+const menu_open = () => {
     $('.c-menu').addClass('is-active');
     $('body').addClass('has-active-menu');
     $('#wrapper').addClass('has-slide-right');
     $('#c-mask').addClass('is-active');
     return true;
 }
-let menu_close = function(){
+const menu_close = () => {
     $('.c-menu').removeClass('is-active');
     $('body').removeClass('has-active-menu');
     $('#wrapper').removeClass('has-slide-right');
@@ -81,31 +75,20 @@ let menu_close = function(){
 
 /*--------HANDLER SETUP-------------*/
 
-$(document).ready(function() {
+document.addEventListener('DOMContentLoaded', () => {
 
     let loc = (window.location.pathname.split('/').length > 0) ? window.location.pathname.split('/').reverse()[0] : '/index.html';
     if (loc == '') loc = '/';
 
     refreshTasks();
 
-    $('.head_entry').css('background-color', $('body').css('background-color'));
-
-    let body = $('#main');
-    body.on("click",function() {
-            showNewCompliment();
-        })
-    body.on("tap", function() {
-            showNewCompliment();
-        })
-    body.on("swiperight", function() {
-            showNewCompliment();
-        })
-    body.on("swipeleft", function() {
+    const body = document.querySelector('#main');
+    body.addEventListener("click",()=> {
             showNewCompliment();
         });
     
-    let menu_btn = $('#menu-opener');
-    menu_btn.click(function(e) {
+    const menu_btn = document.querySelector('#menu-opener');
+    menu_btn.addEventListener("click",(e) =>{
         e.preventDefault();
         if (!menu_state) {
             menu_state = menu_open();
@@ -114,9 +97,9 @@ $(document).ready(function() {
         }
     });
 
-    $('.c-menu__close, .c-mask').click(function(e) {
+    document.querySelector('.c-menu__close, .c-mask').addEventListener("click",(e) => {
         e.preventDefault();
-            menu_state = menu_close();
+        menu_state = menu_close();
     });
 
 });
